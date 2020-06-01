0b97a4d49df43bef87c94f2ddd5a5c906072d6ff
viren4204 <45611825+viren4204@users.noreply.github.com>
2020-06-01 21:16:50 +0530

    use module concept and split code into different files

--- references.js
+++ references.js
@@ -1,26 +1,8 @@
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost/blog_demo_2");
 
-//POST- title, content
-var postSchema = new mongoose.Schema({
-	title: String,
-	content: String
-}); 
-var Post = mongoose.model("Post", postSchema);
-
-//USER- email, name
- var userSchema = new mongoose.Schema({
- 	email: String,
- 	name: String,
- 	posts: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "Post"
-		}
- 	]
- });
- var User = mongoose.model("User", userSchema);
- 
+var Post = require('./models/posts');
+var User = require('./models/user') ;
 // User.create({
 // 	email: "shanti@roy.com",
 // 	name: "Shanti Roy"
@@ -32,36 +14,36 @@ var Post = mongoose.model("Post", postSchema);
 // 	}
 // });
 
-// Post.create({
-// 	title: "how to cook a sandwich",
-// 	content: "its basically simple , but still i don't know how to cook a sandwich. "
-// },function(err, post){
-// 	User.findOne({email:"shanti@roy.com"},function(err, foundUser){
-// 		if (err) {
-// 			console.log(err);
-// 		} else {
-// 			foundUser.posts.push(post);
-// 			foundUser.save(function(err, data){
-// 				if (err) {
-// 					console.log(err);
-// 				} else {
-// 					console.log(data); 
-// 				}
-// 			});
-// 		}
-// 	});
-// });
-//  
+Post.create({
+	title: "how to cook daal bhat",
+	content: "murga bhat khaoge, daal bhat bhul jaoge."
+},function(err, post){
+	User.findOne({email:"shanti@roy.com"},function(err, foundUser){
+		if (err) {
+			console.log(err);
+		} else {
+			foundUser.posts.push(post);
+			foundUser.save(function(err, data){
+				if (err) {
+					console.log(err);
+				} else {
+					console.log(data); 
+				}
+			});
+		}
+	});
+});
+ 
 
 
 
 //find user
-User.findOne({email: "shanti@roy.com"}).populate("posts").exec(function(err, user){
- if (err) {
- 	console.log(err);
- } else {
- 		console.log(user);
- }
-});
+// User.findOne({email: "shanti@roy.com"}).populate("posts").exec(function(err, user){
+//  if (err) {
+//  	console.log(err);
+//  } else {
+//  		console.log(user);
+//  }
+// });
 
 //find all posts for that user
