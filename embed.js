62c9c100fbae20edf97c69e4921a78b8bae4822e
viren4204 <45611825+viren4204@users.noreply.github.com>
2020-06-01 10:43:21 +0530

    embed posts to users data

--- embed.js
+++ embed.js
@@ -1,19 +1,66 @@
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost/blog_demo");
 
-//USER- email, name
- var userSchema = new mongoose.schema({
- 	email: String,
- 	name: String
- });
-
- var User = mongoose.model("User", userSchema);
-
 //POST- title, content
-
-var postSchema = new moongoose.schema({
+var postSchema = new mongoose.Schema({
 	title: String,
 	content: String
-});
-
+}); 
 var Post = mongoose.model("Post", postSchema);
+
+//USER- email, name
+ var userSchema = new mongoose.Schema({
+ 	email: String,
+ 	name: String,
+ 	post: [postSchema]
+ });
+ var User = mongoose.model("User", userSchema);
+
+// var newUser = new User({
+// 	email: 'shubham@nishad.edu',
+// 	name: 'Shubham Nishad'
+// });
+
+// newUser.post.push({
+// 	title:"Bharat mata, is ladke ko kuch nhi ata.",
+// 	content: "just kidding, is ladke ne to bahut machliyaan phasayi hai."
+// });
+ 
+// newUser.save(function(err, user){
+// 	if (err) {
+// 		console.log(err);
+// 	}else {
+// 		console.log(user);
+// 	}
+// });
+
+// var newPost = new Post({
+// 	title: 'Reflections on apple',
+// 	content: "They are delecious"
+// });
+
+// newPost.save(function(err, post){
+// 	if (err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log(post);
+// 	}
+// });
+
+User.findOne({name:"Shubham Nishad"}, function(err, user){
+	if (err){
+		console.log(err);
+	} else {
+		user.post.push({
+			name:"3 things i really hate",
+			content: "cricket, cricket, cricket"
+		});
+		user.save(function(err, user){
+			if (err) {
+				console.log(err);
+			} else {
+				console.log(user);
+			}
+		});
+	}
+});
